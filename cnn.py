230715e070e4a4099455a74140f648b7c6610683
MohammadSarim <52421132+MohammadSarim@users.noreply.github.com>
2020-02-24 16:27:43 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -46,12 +46,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('C:/Users/Muhammad Sarim/Desktop/Machine Learning A-Z New/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/dataset/training_set',
+training_set = train_datagen.flow_from_directory('dataset_path_train',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('C:/Users/Muhammad Sarim/Desktop/Machine Learning A-Z New/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/dataset/test_set',
+test_set = test_datagen.flow_from_directory('dataset_path_test',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
